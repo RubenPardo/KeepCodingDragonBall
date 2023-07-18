ed6902b1366afe02e8ebf9d11eec07ec2e33870f
Ruben Pardo <rubenpardocasanova@gmail.com>
2023-07-18 15:18:38 +0200

    Add Hero Details

--- app/src/main/java/com/example/keepcodingdragonball/presentation/herodetails/HeroDetailsFragment.kt
+++ app/src/main/java/com/example/keepcodingdragonball/presentation/herodetails/HeroDetailsFragment.kt
@@ -2,24 +2,21 @@ package com.example.keepcodingdragonball.presentation.herodetails
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
-import com.example.keepcodingdragonball.R
-import com.example.keepcodingdragonball.domain.model.Hero
+import androidx.fragment.app.Fragment
 import com.example.keepcodingdragonball.core.serializable
-import com.example.keepcodingdragonball.databinding.ActivityHeroListBinding
 import com.example.keepcodingdragonball.databinding.FragmentHeroDetailsBinding
-import com.example.keepcodingdragonball.presentation.herolist.HeroListViewModel
+import com.example.keepcodingdragonball.domain.model.Hero
+import com.squareup.picasso.Picasso
 
 
 private const val ARG_PARAM1 = "hero"
 
 class HeroDetailsFragment : Fragment() {
 
-    private lateinit var param1: Hero
+    private lateinit var hero: Hero
     private lateinit var binding: FragmentHeroDetailsBinding
     //private val viewModel: HeroListViewModel by viewModels()
 
@@ -27,7 +24,7 @@ class HeroDetailsFragment : Fragment() {
         super.onCreate(savedInstanceState)
         arguments?.let { args ->
             args.serializable<Hero>(ARG_PARAM1)?.let { hero ->
-                param1 = hero
+                this.hero = hero
             }
         }
     }
@@ -38,9 +35,14 @@ class HeroDetailsFragment : Fragment() {
     ): View {
         binding = FragmentHeroDetailsBinding.inflate(layoutInflater)
 
-        Log.d("HERO DETAIL", param1.toString())
-
+        setUpViews()
         return binding.root
+    }
+
+    private fun setUpViews() {
+        binding.tvName.text = hero.name
+        binding.tvDescription.text = hero.description
+        Picasso.with(context).load(hero.photo).into(binding.ivHero)
     }
 
     companion object {
